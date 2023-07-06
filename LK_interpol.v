3d38cc16e7f9f302aa86fcbc3d65c9988200784f
asha soroushpoor <ashasoroushpoor@gmail.com>
2023-07-06 19:36:47 +0330

    wth

--- LK_interpol.v
+++ LK_interpol.v
@@ -7,5 +7,68 @@ Theorem LK_Interpol: forall (G1 G2 D1 D2 : list prop),
 G1 ++ G2 ⇒ D1 ++ D2 -> (exists (c : prop), G1 ⇒ D1 ++ c /\ G2 ++ c ⇒ D1).
 intros. induction H.
 - admit.
-- exists a. split.
-    +
+- destruct IHLK as [c [H' H'']]. 
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK1 as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK1 as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+- destruct IHLK1 as [c [H' H'']].
+    exists c. split.
+    + apply H'.
+    + apply H''.
+Admitted.
